04b5743151c6efc6c5ba76bb7e5b91ab9c118270
Abhishek Tomar <itsabhi.at@gmail.com>
2023-04-15 08:04:25 +0530

    new login flow

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,95 +10,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Login Demo',
-      theme: myThemeData,
+      title: 'Flutter Login Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        inputDecorationTheme: InputDecorationTheme(
+          border: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white),
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+      ),
       initialRoute: LoginPage.routeName,
       routes: {
         LoginPage.routeName: (context) => LoginPage(),
         DashboardPage.routeName: (context) => DashboardPage(),
-        LogoutPage.routeName: (context) => LogoutPage(),
+        SignupPage.routeName: (context) => SignupPage(),
       },
     );
   }
 }
 
-final ThemeData myThemeData = ThemeData(
-  colorScheme: ColorScheme.fromSwatch(
-    primarySwatch: Colors.blue,
-    accentColor: Colors.orange,
-  ),
-  backgroundColor: Colors.white,
-  scaffoldBackgroundColor: Colors.white,
-);
-
-// class LoginPage extends StatefulWidget {
-//   static const String routeName = '/';
-
-//   LoginPage({Key? key}) : super(key: key);
-
-//   @override
-//   _LoginPageState createState() => _LoginPageState();
-// }
-
-// class _LoginPageState extends State<LoginPage> {
-//   final _usernameController = TextEditingController();
-//   final _passwordController = TextEditingController();
-
-//   bool _isLoading = false;
-
-//   void _login() {
-//     setState(() {
-//       _isLoading = true;
-//     });
-
-//     // Simulate a network request
-//     Future.delayed(Duration(seconds: 2), () {
-//       setState(() {
-//         _isLoading = false;
-//       });
-//       Navigator.pushNamed(context, DashboardPage.routeName);
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Login'),
-//       ),
-//       body: Padding(
-//         padding: EdgeInsets.all(16),
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             TextFormField(
-//               controller: _usernameController,
-//               decoration: InputDecoration(
-//                 labelText: 'Username',
-//               ),
-//             ),
-//             TextFormField(
-//               controller: _passwordController,
-//               obscureText: true,
-//               decoration: InputDecoration(
-//                 labelText: 'Password',
-//               ),
-//             ),
-//             SizedBox(height: 24),
-//             _isLoading
-//                 ? CircularProgressIndicator()
-//                 : ElevatedButton(
-//                     onPressed: _login,
-//                     child: Text('Login'),
-//                   ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
 class LoginPage extends StatefulWidget {
-  static const String routeName = '/';
+  static const String routeName = '/login';
 
   const LoginPage({Key? key}) : super(key: key);
 
@@ -105,30 +41,20 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  final _usernameController = TextEditingController();
-  final _passwordController = TextEditingController();
-
-  bool _isLoading = false;
+  final _formKey = GlobalKey<FormState>();
   bool _showError = false;
+  bool _isLoading = false;
 
   void _login() {
     setState(() {
       _isLoading = true;
-      _showError = false;
     });
-
-    // Simulate a network request
-    // Future.delayed(Duration(seconds: 2), () {
-    //   setState(() {
-    //     _isLoading = false;
-    //     _showError = true;
-    //   });
-    // });
-    Future.delayed(Duration(seconds: 2), () {
+    // Simulate a login delay
+    Timer(Duration(seconds: 2), () {
       setState(() {
         _isLoading = false;
       });
-      Navigator.pushNamed(context, DashboardPage.routeName);
+      Navigator.of(context).pushReplacementNamed(DashboardPage.routeName);
     });
   }
 
@@ -138,109 +64,121 @@ class _LoginPageState extends State<LoginPage> {
       body: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
+            colors: [Colors.blue, Colors.lightBlueAccent],
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
-            colors: [
-              Color.fromRGBO(110, 109, 222, 1),
-              Color.fromRGBO(36, 11, 138, 1),
-            ],
           ),
         ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Hero(
-                tag: 'logo',
-                child: Image.asset(
-                  'assets/images/logo.png',
-                  height: 100,
-                ),
-              ),
-              SizedBox(height: 32),
-              TextField(
-                controller: _usernameController,
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  labelStyle: TextStyle(
-                    color: Colors.white70,
-                    fontFamily: 'Montserrat',
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: _showError ? Colors.red : Colors.white70,
+        child: Form(
+          key: _formKey,
+          child: Padding(
+            padding: EdgeInsets.all(32),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    labelStyle: TextStyle(
+                      color: Colors.white70,
+                      fontFamily: 'Montserrat',
                     ),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-              ),
-              SizedBox(height: 16),
-              TextField(
-                controller: _passwordController,
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  labelStyle: TextStyle(
-                    color: Colors.white70,
-                    fontFamily: 'Montserrat',
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: _showError ? Colors.red : Colors.white70,
-                    ),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-              ),
-              SizedBox(height: 24),
-              _isLoading
-                  ? CircularProgressIndicator(
-                      color: Colors.white,
-                    )
-                  : SizedBox(
-                      width: double.infinity,
-                      child: ElevatedButton.icon(
-                        onPressed: _login,
-                        icon: Icon(Icons.login),
-                        label: Text('Login'),
-                        style: ElevatedButton.styleFrom(
-                          primary: Color.fromRGBO(255, 255, 255, 0.7),
-                          elevation: 0,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          padding: EdgeInsets.symmetric(vertical: 16),
-                          textStyle: TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'Montserrat',
-                          ),
-                        ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: _showError ? Colors.red : Colors.white70,
                       ),
+                      borderRadius: BorderRadius.circular(10),
                     ),
-              SizedBox(height: 16),
-              GestureDetector(
-                onTap: () {
-                  // Navigator.of(context).pushNamed(SignupPage.routeName);
-                },
-                child: Text(
-                  'Don\'t have an account? Sign up',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontFamily: 'Montserrat',
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your email address';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 16),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    labelStyle: TextStyle(
+                      color: Colors.white70,
+                      fontFamily: 'Montserrat',
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: _showError ? Colors.red : Colors.white70,
+                      ),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your password';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 32),
+                ElevatedButton(
+                  onPressed: _isLoading
+                      ? null
+                      : () {
+                          if (_formKey.currentState!.validate()) {
+                            _login();
+                          } else {
+                            setState(() {
+                              _showError = true;
+                            });
+                          }
+                        },
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(vertical: 16),
+                    child: _isLoading
+                        ? CircularProgressIndicator(
+                            valueColor: AlwaysStoppedAnimation<Color>(
+                                Theme.of(context).primaryColor),
+                          )
+                        : Text(
+                            'Login',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
                   ),
                 ),
-              ),
-            ],
+                SizedBox(height: 16),
+                TextButton(
+                  onPressed: _isLoading
+                      ? null
+                      : () {
+                          Navigator.of(context).pushNamed(SignupPage.routeName);
+                        },
+                  child: Text(
+                    'Don\'t have an account? Sign up',
+                    style: TextStyle(
+                      color: Colors.white70,
+                      fontFamily: 'Montserrat',
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -251,53 +189,47 @@ class _LoginPageState extends State<LoginPage> {
 class DashboardPage extends StatelessWidget {
   static const String routeName = '/dashboard';
 
+  const DashboardPage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Dashboard'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () {
+              Navigator.of(context).pushReplacementNamed(LoginPage.routeName);
+            },
+          ),
+        ],
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Welcome!', style: Theme.of(context).textTheme.headline4),
-            SizedBox(height: 24),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, LogoutPage.routeName);
-              },
-              child: Text('Logout'),
-            ),
-          ],
+        child: Text(
+          'Welcome to the dashboard!',
+          style: TextStyle(fontSize: 24),
         ),
       ),
     );
   }
 }
 
-class LogoutPage extends StatelessWidget {
-  static const String routeName = '/logout';
+class SignupPage extends StatelessWidget {
+  static const String routeName = '/signup';
+
+  const SignupPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Logout'),
+        title: Text('Sign Up'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('You have been logged out.'),
-            SizedBox(height: 24),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, LoginPage.routeName);
-              },
-              child: Text('Login again'),
-            ),
-          ],
+        child: Text(
+          'Sign Up Page',
+          style: TextStyle(fontSize: 24),
         ),
       ),
     );
